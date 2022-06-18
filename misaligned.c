748af51ac0279d550c365bddd6b169185df1d358
Gopinath <25692465+devgopinath@users.noreply.github.com>
2022-06-18 12:33:25 +0530

    separated function get the details and excusive to print and test

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+typedef struct
+{
+    int pairNumber;
+    const char * majorColor;
+    const char * minorColor;
+} colorMapStruct;
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+colorMapStruct ColorMaps[30];
+
+
+int GetColorMap(colorMapStruct * colorMaps)
+{
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            colorMaps.pairNumber = i * 5 + j;
+            colorMaps.pairNumber = majorColor[i];
+            colorMaps.pairNumber = minorColor[i];
         }
     }
     return i * j;
 }
 
-int main() {
-    int result = printColorMap();
-    assert(result == 25);
+void printColorMap()
+{
+    int numCombinations = GetColorMap(ColorMaps);
+    for (int index = 0; index < numCombinations; ++index)
+    {
+        printf("%d | %s | %s\n", ColorMaps[index].pairNumber, ColorMaps[index].majorColor, ColorMaps[index].minorColor);
+    }
+}
+
+void testColorMap(void)
+{
+    int numCombinations = GetColorMap(ColorMaps);
+    for (int index = 0, pairNumber = 1; index < numCombinations; ++index, ++pairNumber)
+    {
+        assert(pairNumber == ColorMaps[index].pairNumber);
+        assert(majorColor[index] == ColorMaps[index].majorColor);
+        assert(minorColor[index] == ColorMaps[index].minorColor);
+    }
     printf("All is well (maybe!)\n");
+}
+
+int main() {
+    printColorMap();
+    testColorMap();
     return 0;
 }
