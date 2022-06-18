93ad9159c940ecabb084995056104c9886b005fa
Gopinath <25692465+devgopinath@users.noreply.github.com>
2022-06-18 11:08:06 +0530

    added test cases for tshirts

--- tshirts.c
+++ tshirts.c
@@ -14,9 +14,18 @@ char size(int cms) {
 }
 
 int main() {
+    /* classify as 'S', if length is less than or equal to 38 */
     assert(size(37) == 'S');
+    assert(size(38) == 'S');
+
+    /* classify as 'M', if length is greater than 38 and less than or equal to 42 */
+    assert(size(39) == 'M');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
+
+    /* classify as 'L', if length is greater than 43 */
     assert(size(43) == 'L');
+
     printf("All is well (maybe!)\n");
     return 0;
 }
