8ba856e311584a6b9636592d9d775d1db0934e6d
Gopinath <25692465+devgopinath@users.noreply.github.com>
2022-06-27 19:44:44 +0530

    added alignment

--- misaligned.c
+++ misaligned.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 typedef struct
 {
@@ -11,10 +12,22 @@ typedef struct
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
+int maxMajorColorStrLength;
+int maxPairNumberStrLength;
+
 #define MAX_MAJOR_COLORS    (sizeof(majorColor)/sizeof(majorColor[0]))
 #define MAX_MINOR_COLORS    (sizeof(minorColor)/sizeof(minorColor[0]))
 
 #define MAX_COMBINATIONS    (MAX_MAJOR_COLORS * MAX_MINOR_COLORS)
+
+#define EMPTY_STRING    ("")
+#define SEPARATOR_STRING    (" | ")
+#define SPACE_STRING (" ")
+
+#define EXPECTED_STR_LENGTH_AFTER_PAIR_NUMBER (maxPairNumberStrLength)
+#define EXPECTED_STR_LENGTH_AFTER_MAJOR_COLOR (EXPECTED_STR_LENGTH_AFTER_PAIR_NUMBER + \
+                                               strlen(SEPARATOR_STRING) + \
+                                               maxMajorColorStrLength)
 
 unsigned int getPairNumber(unsigned int majorColorIndex, unsigned int minorColorIndex)
 {
@@ -40,16 +53,63 @@ unsigned int GetColorMap(colorMapStruct * colorMaps)
     return majorColorIndex * minorColorIndex;
 }
 
+int getMaxMajorColorStrLength(void)
+{
+    int maxLength = 0;
+    int length;
+    for (int index = 0; index < MAX_MAJOR_COLORS; ++index)
+    {
+        length = strlen(majorColor[index]);
+        if (length > maxLength)
+        {
+            maxLength = length;
+        }
+    }
+    return maxLength;
+}
+int getMaxPairNumberStrLength(void)
+{
+    char buffer[10];
+    sprintf(buffer, "%d", (MAX_COMBINATIONS));
+    return (strlen(buffer));
+}
+
+void addSpaces(char * str, int expectedStringLength)
+{
+    int length = strlen(str);
+
+    while (length < expectedStringLength)
+    {
+        strcat(str, SPACE_STRING);
+        ++length;
+    }
+}
+
+void getColorCodeAlignedString(int pairNumber, const char * majorColor, const char * minorColor, char * alignedString)
+{
+    strcpy(alignedString, EMPTY_STRING);
+    strcat(alignedString, pairNumber);
+    addSpaces(alignedString, EXPECTED_STR_LENGTH_AFTER_PAIR_NUMBER);
+    strcat(alignedString, SEPARATOR_STRING);
+    strcat(alignedString, majorColor);
+    addSpaces(alignedString, EXPECTED_STR_LENGTH_AFTER_MAJOR_COLOR);
+    strcat(alignedString, SEPARATOR_STRING);
+    strcat(alignedString, minorColor);
+}
+
 void printColorMap()
 {
     colorMapStruct ColorMaps[MAX_COMBINATIONS];
+    char alignedString[25];
     unsigned int numCombinations = GetColorMap(ColorMaps);
 
     for (unsigned int colorMapIndex = 0; colorMapIndex < numCombinations; ++colorMapIndex)
     {
-        printf("%d | %s | %s\n", ColorMaps[colorMapIndex].pairNumber, 
-                                 ColorMaps[colorMapIndex].majorColor, 
-                                 ColorMaps[colorMapIndex].minorColor);
+        getColorCodeAlignedString(ColorMaps[colorMapIndex].pairNumber, 
+                                  ColorMaps[colorMapIndex].majorColor, 
+                                  ColorMaps[colorMapIndex].minorColor,
+                                  alignedString);
+        printf("%s\n", alignedString);
     }
 }
 
@@ -72,6 +132,8 @@ void testColorMap(void)
 
 int main()
 {
+    maxMajorColorStrLength = getMaxMajorColorStrLength();
+    maxPairNumberStrLength = getMaxPairNumberStrLength();
     printColorMap();
     testColorMap();
     return 0;
